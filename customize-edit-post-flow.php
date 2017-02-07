121a84bd6df0e301fbc5c42e125bae1d241e915b
Matt Wiebe <wiebe@automattic.com>
2017-02-07 09:54:02 -0600

    Full working flow, with post edit screen notices

--- customize-edit-post-flow.php
+++ customize-edit-post-flow.php
@@ -44,6 +44,24 @@ class WP_Customize_Post_Edit_Flow {
 	private function __construct() {
 		add_action( 'customize_register', array( $this, 'customizer_init' ) );
 		add_action( 'edit_form_top', array( $this, 'post_edit_notice' ) );
+		add_filter( 'redirect_post_location', array( $this, 'maybe_add_return_to_redirect' ) );
+		add_action( 'edit_form_top', array( $this, 'maybe_add_hidden_field' ) );
+	}
+
+	public function maybe_add_hidden_field() {
+		if ( ! isset( $_GET['customizer_return'] ) || ! $this->is_customizer_url( $_GET['customizer_return'] ) ) {
+			return;
+		}
+
+		printf( '<input type="hidden" name="%s" value="%s" />', 'customizer_return', esc_attr( $_GET['customizer_return'] ) );
+	}
+
+	public function maybe_add_return_to_redirect( $location ) {
+		if ( isset( $_POST['customizer_return'] ) ) {
+			// todo verify _return_to_customizer_url
+			$location = add_query_arg( 'customizer_return', $_POST['customizer_return'], $location );
+		}
+		return $location;
 	}
 
 	public function customizer_init() {
@@ -60,12 +78,48 @@ class WP_Customize_Post_Edit_Flow {
 		wp_enqueue_script( 'edit-post-flow', plugins_url( 'js/customize-edit-post-flow-admin.js', __FILE__ ), array( 'customize-controls' ), '20170201', true );
 	}
 
+	private function did_get_refered_from_customizer() {
+		if ( empty( $_SERVER['HTTP_REFERER'] ) ) {
+			return false;
+		}
+
+		return $this->is_customizer_url( $_SERVER['HTTP_REFERER'] );
+	}
+
+	private function has_customizer_return() {
+		if ( empty( $_REQUEST['customizer_return'] ) ) {
+			return false;
+		}
+
+		return $this->is_customizer_url( $_REQUEST['customizer_return'] );
+	}
+
+	private function is_customizer_url( $url ) {
+		$parsed = parse_url( $url );
+
+		if ( empty( $parsed['path'] ) || empty( $parsed['scheme'] || empty( $parsed['host'] ) ) ) {
+			return false;
+		}
+
+		return admin_url( 'customize.php' ) === sprintf( '%s://%s%s', $parsed['scheme'], $parsed['host'], $parsed['path'] );
+	}
+
 	public function post_edit_notice() {
-		if ( ! isset( $_GET['return_uuid'] ) ) {
+		// If no return URL, bail
+		if ( ! $this->has_customizer_return() ) {
 			return;
 		}
+
 		$post_object = get_post_type_object( get_post()->post_type );
-		$message = sprintf( __( 'After you finish editing this %s, you can return to customizing your site\'s appearance.'  ), $post_object->labels->singular_name );
+
+		// If we just came from the Customizer, show a notice of state
+		if ( $this->did_get_refered_from_customizer() ) {
+			$message = sprintf( __( 'After you finish editing this %s, you can return to customizing your site\'s appearance.'  ), $post_object->labels->singular_name );
+		} else {
+			// otherwise, we're ready to go back.
+			$message = sprintf( __( 'You edited your %s!' ), $post_object->labels->singular_name );
+			$message .= sprintf( ' <a href="%s">%s</a>', esc_url( $_REQUEST['customizer_return'] ), __( 'Continue customizing your site.' ) );
+		}
 		echo '<div class="notice notice-warning is-dismissible"><p>' . $message . '</p></div>';
 	}
 
--- js/customize-edit-post-flow-admin.js
+++ js/customize-edit-post-flow-admin.js
@@ -1,13 +1,36 @@
 (function( wp, $ ) {
 	var api = wp.customize;
 
-	api.bind( 'ready', bindIt );
+	api.postEditRedirect = function postEditRedirect( url ) {
+		if ( api.state( 'saved' ).get() ) {
+			redirectWithReturn( url );
+		}
 
-	function bindIt() {
-		api.previewer.bind( 'edit-post', (url) => {
-			window.location = url + '&return_uuid=' + api.settings.changeset.uuid;
-		} );
+		if ( ! api.state( 'changesetStatus' ).get() ) {
+			api.state( 'changesetStatus' ).bind( ( newStatus ) => {
+				if ( newStatus = 'auto-draft' ) {
+					unbindAYS();
+					redirectWithReturn( url );
+				}
+			});
+		} else {
+			redirectWithReturn( url );
+		}
 	}
 
+	function addCustomizerReturnUrl( url ) {
+		var sep = ( url.indexOf( '?' ) >= 0 ) ? '&' : '?';
+		return url + sep + 'customizer_return=' + encodeURIComponent( window.location.href );
+	}
+
+	function redirectWithReturn( url ) {
+		window.location = addCustomizerReturnUrl( url );
+	}
+
+	function unbindAYS() {
+		$( window ).off( 'beforeunload.customize-confirm' );
+	}
+
+	api.bind( 'ready', () => api.previewer.bind( 'edit-post', api.postEditRedirect ) );
 
 })( this.wp, this.jQuery );
